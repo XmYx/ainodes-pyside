1dd2103ce968aef1c87db2635deb5a720a5507a8
Werner Oswald <osiworx@gmail.com>
2022-11-24 19:26:43 +0100

    added lookup for IP in fast_app.py

--- apis/fast_app.py
+++ apis/fast_app.py
@@ -64,4 +64,4 @@ ngrok_tunnel = ngrok.connect(8000)
 print('Public URL:', ngrok_tunnel.public_url)
 nest_asyncio.apply()
 
-uvicorn.run(app, port=8080, log_level="debug")
+uvicorn.run(app, port=8000, log_level="debug")

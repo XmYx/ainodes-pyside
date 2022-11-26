a4bf290f110877de3399cc612c758ba28861b15b
XmYx <miklos.mnagy@gmail.com>
2022-11-26 17:39:03 +0100

    vae loading
    put vae next to model, and rename to <model_name>.pt

--- backend/deforum/deforum_adapter.py
+++ backend/deforum/deforum_adapter.py
@@ -31,6 +31,22 @@ from backend.singleton import singleton
 
 gs = singleton
 
+vae_ignore_keys = {"model_ema.decay", "model_ema.num_updates"}
+
+
+default_vae_dict = {"auto": "auto", "None": "None"}
+default_vae_list = ["auto", "None"]
+
+
+default_vae_values = [default_vae_dict[x] for x in default_vae_list]
+vae_dict = dict(default_vae_dict)
+vae_list = list(default_vae_list)
+first_load = True
+
+
+base_vae = None
+loaded_vae_file = None
+checkpoint_info = None
 def load_model_from_config_lm(ckpt, verbose=False):
     print(f"Loading model from {ckpt}")
     pl_sd = torch.load(ckpt, map_location="cpu")
@@ -192,8 +208,39 @@ class DeforumSix:
             # todo why we do this here?
             from  backend.aesthetics import modules
             print('PersonalizedCLIPEmbedder', backend.aesthetics.modules.PersonalizedCLIPEmbedder)
+            vae_name = os.path.splitext(gs.system.sdPath)[0] + '.pt'
+            self.load_vae(vae_name)
 
+    def load_vae(self, vae_file=None):
+        global first_load, vae_dict, vae_list, loaded_vae_file
+        # save_settings = False
 
+        if vae_file:
+            assert os.path.isfile(vae_file), f"VAE file doesn't exist: {vae_file}"
+            print(f"Loading VAE weights from: {vae_file}")
+            vae_ckpt = torch.load(vae_file, map_location='cpu')
+            vae_dict_1 = {k: v for k, v in vae_ckpt["state_dict"].items() if
+                          k[0:4] != "loss" and k not in vae_ignore_keys}
+            load_vae_dict(gs.models["sd"], vae_dict_1)
+
+            # If vae used is not in dict, update it
+            # It will be removed on refresh though
+            #vae_opt = get_filename(vae_file)
+            #if vae_opt not in vae_dict:
+            #    vae_dict[vae_opt] = vae_file
+            #    vae_list.append(vae_opt)
+
+        loaded_vae_file = vae_file
+
+        """
+        # Save current VAE to VAE settings, maybe? will it work?
+        if save_settings:
+            if vae_file is None:
+                vae_opt = "None"
+            # shared.opts.sd_vae = vae_opt
+        """
+
+        first_load = False
 
 
     def load_model(self):
@@ -1024,3 +1071,36 @@ def make_batch_sd(
         "masked_image": repeat(masked_image.to(device=device), "1 ... -> n ...", n=num_samples),
     }
     return batch
+
+def load_vae_dict(model, vae_dict_1=None):
+    if vae_dict_1:
+        store_base_vae(model)
+        model.first_stage_model.load_state_dict(vae_dict_1)
+    else:
+        restore_base_vae()
+    model.first_stage_model.to('cuda')
+
+def get_base_vae(model):
+    if base_vae is not None and checkpoint_info == model.sd_checkpoint_info and model:
+        return base_vae
+    return None
+
+
+def store_base_vae(model):
+    global base_vae #, checkpoint_info
+    #if checkpoint_info != model.sd_checkpoint_info:
+    base_vae = model.first_stage_model.state_dict().copy()
+        #checkpoint_info = model.sd_checkpoint_info
+
+
+def delete_base_vae():
+    global base_vae, checkpoint_info
+    base_vae = None
+    checkpoint_info = None
+
+
+def restore_base_vae(model):
+    global base_vae, checkpoint_info
+    if base_vae is not None and checkpoint_info == model.sd_checkpoint_info:
+        load_vae_dict(model, base_vae)
+    delete_base_vae()

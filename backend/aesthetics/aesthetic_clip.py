c496f49c121b9d72e92882ae36b979773844c4d1
XmYx <miklos.mnagy@gmail.com>
2022-12-02 14:17:47 +0100

    test

--- backend/aesthetics/aesthetic_clip.py
+++ backend/aesthetics/aesthetic_clip.py
@@ -167,7 +167,7 @@ class AestheticCLIP:
         if image_embs_name is None or len(image_embs_name) == 0 or image_embs_name == "None":
             image_embs_name = None
             self.image_embs_name = None
-        if image_embs_name is not None and self.image_embs_name != image_embs_name:
+        if image_embs_name is not None and self.image_embs_name != image_embs_name and gs.aesthetic_embedding_path != None:
             self.image_embs_name = image_embs_name
             self.image_embs = torch.load(gs.aesthetic_embedding_path, map_location=self.device)
             self.image_embs /= self.image_embs.norm(dim=-1, keepdim=True)

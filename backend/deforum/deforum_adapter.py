fixed some bug in update modellist added more params for aesthetic gradients and mad vae and hypernetworks useable from the ui

backend/deforum/deforum_adapter.py
@@ -113,17 +113,6 @@ class DeforumSix:
             del lo
             del sd
 
-
-    def run_pre_load_model_generation_specifics(self, config):
-
-
-        if gs.model_version in gs.system.gen_one_models and 2==1:
-            config.model.params.cond_stage_config.params.T = 10
-            config.model.params.cond_stage_config.params.lr = 0.0001
-            config.model.params.cond_stage_config.params.aesthetic_embedding_path = (
-                "models/embeddings/discostyle.pt"
-            )
-
     def run_post_load_model_generation_specifics(self):
 
         #print("Loading Hypaaaa")
@@ -192,8 +181,6 @@ class DeforumSix:
             if verbose:
                 print(gs.model_version)
 
-            self.run_pre_load_model_generation_specifics(config)
-
 
             pl_sd = torch.load(ckpt, map_location="cpu")
             if "global_step" in pl_sd:
@@ -241,10 +228,8 @@ class DeforumSix:
             gs.models["sd"].to(self.device)
             # todo why we do this here?
             from backend.aesthetics import modules
-            #print('PersonalizedCLIPEmbedder', backend.aesthetics.modules.PersonalizedCLIPEmbedder)
-            # todo make this vae thing a setting
-            vae_name = os.path.splitext(gs.system.sdPath)[0] + '.pt'
-            self.load_vae(vae_name)
+            if gs.diffusion.selected_vae != 'None':
+                self.load_vae(gs.diffusion.selected_vae)
 
     def load_vae(self, vae_file=None):
         global first_load, vae_dict, vae_list, loaded_vae_file
@@ -496,7 +481,7 @@ class DeforumSix:
                         prompt="",
                         negative_prompts=None,
                         hires=None,
-                        use_hypernetwork=None,
+                        #use_hypernetwork=None,
                         apply_strength=0,
                         apply_circular=False,
                         lowmem=False
@@ -569,8 +554,8 @@ class DeforumSix:
 
 
 
-        if use_hypernetwork is not None:
-            hypernetwork.load_hypernetwork(use_hypernetwork)
+        if gs.diffusion.selected_hypernetwork != 'None':
+            hypernetwork.load_hypernetwork(gs.diffusion.selected_hypernetwork)
             hypernetwork.apply_strength(apply_strength)                          #1.0, "Hypernetwork strength", gr.Slider, {"minimum": 0.0, "maximum": 1.0, "step": 0.001}),
             gs.model_hijack.apply_circular(False)
             gs.model_hijack.clear_comments()
@@ -594,7 +579,16 @@ class DeforumSix:
             except:
                 pass"""
 
-        gs.models["sd"].cond_stage_model.process_tokens.set_aesthetic_params()
+        if gs.diffusion.selected_aesthetic_embedding != 'None':
+            gs.models["sd"].cond_stage_model.process_tokens.set_aesthetic_params(
+                esthetic_lr=gs.lr,
+                aesthetic_weight=gs.aesthetic_weight,
+                aesthetic_steps=gs.T,
+                image_embs_name=gs.diffusion.selected_aesthetic_embedding,
+                aesthetic_slerp=gs.slerp,
+                aesthetic_imgs_text=gs.aesthetic_imgs_text,
+                aesthetic_slerp_angle=gs.slerp_angle,
+                aesthetic_text_negative=gs.aesthetic_text_negative)
 
         if hires:
             args.hiresstr = args.strength

frontend/mainwindow.py
@@ -151,8 +151,15 @@ class MainWindow(QMainWindow):
         self.connections()
         self.list_files()
         self.resize(1280, 800)
+        self.create_sys_folders()
+
         self.widgets[self.current_widget].update_model_list()
-        self.create_out_folders()
+        self.widgets[self.current_widget].update_vae_list()
+        self.widgets[self.current_widget].update_hypernetworks_list()
+        self.widgets[self.current_widget].update_aesthetics_list()
+
+
+
         check_models_exist()
         self.latent_rgb_factors = torch.tensor([
             #   R        G        B
@@ -162,7 +169,7 @@ class MainWindow(QMainWindow):
             [-0.184, -0.271, -0.473],  # L4
         ], dtype=torch.float, device='cuda')
 
-    def create_out_folders(self):
+    def create_sys_folders(self):
         os.makedirs(gs.system.galleryMainPath, exist_ok=True)
         os.makedirs(gs.system.txt2imgOut, exist_ok=True)
         os.makedirs(gs.system.img2imgTmp, exist_ok=True)
@@ -173,6 +180,13 @@ class MainWindow(QMainWindow):
         os.makedirs(gs.system.vid2vidSingleFrame, exist_ok=True)
         os.makedirs(gs.system.vid2vidOut, exist_ok=True)
         os.makedirs(gs.system.customModels, exist_ok=True)
+        os.makedirs(gs.system.default_config_yaml_path, exist_ok=True)
+        os.makedirs(gs.system.vae_path, exist_ok=True)
+        os.makedirs(gs.system.hypernetwork, exist_ok=True)
+        os.makedirs(gs.system.hypernetwork_dir, exist_ok=True)
+
+
+
 
     def connections(self):
         self.deforum_ui.signals.txt2img_image_cb.connect(self.image_preview_func)
@@ -1184,16 +1198,16 @@ class MainWindow(QMainWindow):
 
     def list_files(self, index=0):
         paths = []
-        self.widgets[self.current_widget].w.aesthetic_embedding.clear()
-        self.widgets[self.current_widget].w.aesthetic_embedding.addItem("None")
+        self.widgets[self.current_widget].w.select_aesthetic_embedding.clear()
+        self.widgets[self.current_widget].w.select_aesthetic_embedding.addItem("None")
         for _, _, files in os.walk(gs.system.aesthetic_gradients):
             for file in files:
-                self.widgets[self.current_widget].w.aesthetic_embedding.addItem(str(file))
+                self.widgets[self.current_widget].w.select_aesthetic_embedding.addItem(str(file))
         #self.set_txt2img.w.gradientList.setItemText(index)
 
     def select_gradient(self, gradient):
-        if self.widgets[self.current_widget].w.aesthetic_embedding.itemText(gradient) != "None":
-            gs.aesthetic_embedding_path = os.path.join(gs.system.aesthetic_gradients, self.widgets[self.current_widget].w.aesthetic_embedding.itemText(gradient))
+        if self.widgets[self.current_widget].w.select_aesthetic_embedding.itemText(gradient) != "None":
+            gs.aesthetic_embedding_path = os.path.join(gs.system.aesthetic_gradients, self.widgets[self.current_widget].w.select_aesthetic_embedding.itemText(gradient))
         else:
             gs.aesthetic_embedding_path = None
 
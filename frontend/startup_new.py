c0aca3b84fce2a2480a34524303541252dbeeed3
XmYx <miklos.mnagy@gmail.com>
2022-12-17 23:02:32 +0100

    Create startup_new.py

---
+++ frontend/startup_new.py
@@ -0,0 +1,40 @@
+import sys
+
+from PySide6 import QtCore, QtQuick
+from PySide6.QtGui import QIcon, QAction, QPixmap
+
+from PySide6.QtQuick import QSGRendererInterface
+from PySide6.QtWidgets import QApplication, QSplashScreen
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+sys.path.append('src/AdaBins')
+sys.path.append('src/MiDaS')
+sys.path.append('src/pytorch3d-lite')
+sys.path.append('src/BLIP')
+def run_app():
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_ShareOpenGLContexts)
+    QtQuick.QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGLRhi)
+    #app = QApplication(sys.argv)
+
+
+    pixmap = QPixmap('frontend/main/splash_2.png')
+    splash = QSplashScreen(pixmap)
+    splash.show()
+    icon = QIcon('frontend/main/splash_2.png')
+    from frontend.mainwindow import MainWindow
+
+    sshFile="frontend/style/QTDark.stylesheet"
+
+    mainWindow = MainWindow()
+
+    mainWindow.setWindowTitle("aiNodes")
+    mainWindow.setWindowIcon(QIcon('frontend/main/splash_2.png'))
+    with open(sshFile,"r") as fh:
+        mainWindow.setStyleSheet(fh.read())
+
+    mainWindow.show()
+
+    splash.finish(mainWindow)
+
+    #sys.exit(app.exec())

0aad36d4765e13768f4454fcf3ef2f141b8f17cb
XmYx <miklos.mnagy@gmail.com>
2022-12-19 21:06:13 +0100

    test #2

--- plugins/training/callbacks.py
+++ plugins/training/callbacks.py
@@ -211,9 +211,9 @@ class ImageLogger(Callback):
         self.rescale = rescale
         self.batch_freq = batch_frequency
         self.max_images = max_images
-        self.logger_log_images = {
-            pl.loggers.TestTubeLogger: self._testtube,
-        }
+        #self.logger_log_images = {
+        #    pl.loggers.TestTubeLogger: self._testtube,
+        #}
         self.log_steps = [2 ** n for n in range(int(np.log2(self.batch_freq)) + 1)]
         if not increase_log_steps:
             self.log_steps = [self.batch_freq]
@@ -280,8 +280,8 @@ class ImageLogger(Callback):
             self.log_local(pl_module.logger.save_dir, split, images,
                            pl_module.global_step, pl_module.current_epoch, batch_idx)
 
-            logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
-            logger_log_images(pl_module, images, pl_module.global_step, split)
+            #logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
+            #logger_log_images(pl_module, images, pl_module.global_step, split)
 
             if is_train:
                 pl_module.train()

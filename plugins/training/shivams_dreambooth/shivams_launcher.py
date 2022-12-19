e75fe456efb322cc37d9bf2032a4e9f400e87a1a
XmYx <miklos.mnagy@gmail.com>
2022-12-19 21:57:01 +0100

    Update shivams_launcher.py

--- plugins/training/shivams_dreambooth/shivams_launcher.py
+++ plugins/training/shivams_dreambooth/shivams_launcher.py
@@ -41,7 +41,7 @@ def run_shivams_dreambooth():
 
     command = ['accelerate', 'launch', 'plugins/training/shivams_dreambooth/dreambooth.py',
                '--pretrained_model_name_or_path=$MODEL_NAME',
-               '--pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse"',
+               '--pretrained_vae_name_or_path=stabilityai/sd-vae-ft-mse',
                '--output_dir=$OUTPUT_DIR',
                '--revision=fp16',
                '--with_prior_preservation', '--prior_loss_weight=1.0',
@@ -53,14 +53,14 @@ def run_shivams_dreambooth():
                '--use_8bit_adam',
                '--gradient_accumulation_steps=1',
                '--learning_rate=1e-6',
-               '--lr_scheduler="constant"',
+               '--lr_scheduler=constant',
                '--lr_warmup_steps=0',
                '--num_class_images=50',
                '--sample_batch_size=4',
                '--max_train_steps=800',
                '--save_interval=10000',
-               '--save_sample_prompt="photo of zwx dog"',
-               '--concepts_list="concepts_list.json"']
+               '--save_sample_prompt=photo of zwx dog',
+               '--concepts_list=concepts_list.json']
 
     subprocess.run(command)
 

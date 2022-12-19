53ecc33a3112554076a0be8200fde1449924ce08
XmYx <miklos.mnagy@gmail.com>
2022-12-19 21:53:04 +0100

    formatting

--- plugins/training/shivams_dreambooth/shivams_launcher.py
+++ plugins/training/shivams_dreambooth/shivams_launcher.py
@@ -39,29 +39,28 @@ def run_shivams_dreambooth():
     with open("concepts_list.json", "w") as f:
         json.dump(concepts_list, f, indent=4)
 
+    command = ['accelerate', 'launch', 'plugins/training/shivams_dreambooth/dreambooth.py',
+               '--pretrained_model_name_or_path=$MODEL_NAME',
+               '--pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse"',
+               '--output_dir=$OUTPUT_DIR',
+               '--revision="fp16"',
+               '--with_prior_preservation', '--prior_loss_weight=1.0',
+               '--seed=1337',
+               '--resolution=512',
+               '--train_batch_size=1',
+               '--train_text_encoder',
+               '--mixed_precision="fp16"',
+               '--use_8bit_adam',
+               '--gradient_accumulation_steps=1',
+               '--learning_rate=1e-6',
+               '--lr_scheduler="constant"',
+               '--lr_warmup_steps=0',
+               '--num_class_images=50',
+               '--sample_batch_size=4',
+               '--max_train_steps=800',
+               '--save_interval=10000',
+               '--save_sample_prompt="photo of zwx dog"',
+               '--concepts_list="concepts_list.json"']
 
-    cmd = 'accelerate launch plugins/training/shivams_dreambooth/dreambooth.py \
-      --pretrained_model_name_or_path=$MODEL_NAME \
-      --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
-      --output_dir=$OUTPUT_DIR \
-      --revision="fp16" \
-      --with_prior_preservation --prior_loss_weight=1.0 \
-      --seed=1337 \
-      --resolution=512 \
-      --train_batch_size=1 \
-      --train_text_encoder \
-      --mixed_precision="fp16" \
-      --use_8bit_adam \
-      --gradient_accumulation_steps=1 \
-      --learning_rate=1e-6 \
-      --lr_scheduler="constant" \
-      --lr_warmup_steps=0 \
-      --num_class_images=50 \
-      --sample_batch_size=4 \
-      --max_train_steps=800 \
-      --save_interval=10000 \
-      --save_sample_prompt="photo of zwx dog" \
-      --concepts_list="concepts_list.json"'
-
-    subprocess.run([cmd])
+    subprocess.run(command)
 

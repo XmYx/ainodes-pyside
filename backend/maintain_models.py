08b3fb3a209f1f0c9330f3d573a609b9afb2ddaa
Werner Oswald <osiworx@gmail.com>
2023-01-29 18:16:26 +0100

    add retry when downloading the support models at first install. also It tells what happens if the download fails anyways

--- backend/maintain_models.py
+++ backend/maintain_models.py
@@ -25,8 +25,25 @@ def check_models_exist():
             if not os.path.isfile(model_path[key]['path']):
                 url = model_path[key]['url']
                 print(f"Downloading {path} model from {url}")
-                wget(model_path[key]['url'], model_path[key]['path'])
-                #subprocess.run(["curl", "-L", model_path[key]['url'], "-o", model_path[key]['path']])
+                print('The download may take a while please be patient there is no progress update here')
+                n = 0
+                check = 0
+                while n < 3:
+                    try:
+                        wget(model_path[key]['url'], model_path[key]['path'])
+                        check = 1
+                        break
+                    except Exception as e:
+                        n = n + 1
+                        print(f'there was an error, we will try again {3-n} times')
+                    finally:
+                        if check == 1:
+                            print('the download was successfull')
+                        else:
+                            print(f"Downloading {path} model from {url} was no success, aiNodes will work anyways for the most part but be aware that once you try to use that model you might have to download it first")
+                            print('The internet is a non predictable place. we did try 3 times to download the file and failed this can have a multitude of reasons which we can not estimate here')
+                            print('If you try again next time to run setup.bat we will try again to download the missing models for you')
+
         if 'hint' in model_path[key]:
             if not os.path.isfile(model_path[key]['path']):
                 print(f"You may have to download the model supposed to be at {model_path[key]['path']} from here: {model_path[key]['hint']}")

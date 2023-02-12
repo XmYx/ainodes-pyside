d49f6d431410d98a3714959144d5a7aeb520ac6d
Werner Oswald <osiworx@gmail.com>
2023-02-12 09:42:29 +0100

    less bugs, clickable start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,35 @@
+@echo off
+
+rem Check for Anaconda in the system profile
+if exist "%ProgramData%\Anaconda3\Scripts\conda.bat" (
+  call "%ProgramData%\Anaconda3\Scripts\conda.bat" activate ai-nodes
+  goto end
+)
+
+rem Check for Anaconda in the user profile
+if exist "%userprofile%\Anaconda3\Scripts\conda.bat" (
+  call "%userprofile%\Anaconda3\Scripts\conda.bat" activate ai-nodes
+  goto end
+)
+
+rem Check for Miniconda in the system profile
+if exist "%ProgramData%\Miniconda3\Scripts\activate.bat" (
+  call "%ProgramData%\Miniconda3\Scripts\activate.bat" activate ai-nodes
+  goto end
+)
+
+rem Check for Miniconda in the user profile
+if exist "%userprofile%\Miniconda3\Scripts\activate.bat" (
+  call "%userprofile%\Miniconda3\Scripts\activate.bat" activate ai-nodes
+  goto end
+)
+echo Error: Conda was not found in either the system or user profile.
+pause
+goto end
+
+:end
+
+
 set PYSIDE_DESIGNER_PLUGINS = '.'
 
 call activate ai-nodes
